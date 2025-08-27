61e8b1937434a3c246608d8b404cfa5a070c4de7
Churchill <reghoyah@gmail.com>
2025-08-27 22:53:11 +0000

    changed the props for the component per what the checker wants

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile name='Hoyah Churchill' age={23} bio='Im a software engineer' />
+      <UserProfile name='Alice' age={25} bio='Loves hiking and photography' />
     </>
   )
 }
