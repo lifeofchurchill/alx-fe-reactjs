030375cfbbb91dd4f0d803786c2469070d1a6c77
Churchill <reghoyah@gmail.com>
2025-09-12 00:16:19 +0000

    adding a margin style as ALX requested

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 const UserProfile = (props) => {
     return (
-        <div style={{ border: '1px solid gray', padding: '10px' }}>
+        <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
             <h2 style={{ color:'blue' }}>Hello, {props.name} </h2>
             <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
             <p>Bio: {props.bio}</p>
