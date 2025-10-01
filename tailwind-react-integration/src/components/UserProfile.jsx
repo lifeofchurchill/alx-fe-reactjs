9f8d6951b063f988d4f15f485548d908bcaad8f9
Churchill <reghoyah@gmail.com>
2025-10-01 15:36:39 +0000

    interactivity and transition with tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
       <img src="https://via.placeholder.com/150" alt="User" 
-      className="rounded-full md:w-36 md:h-36 sm:h-24 sm:w-24 mx-auto " />
-      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">John Doe</h1>
+      className="rounded-full md:w-36 md:h-36 sm:h-24 sm:w-24 mx-auto hover:scale-100 transition-transform duration-300 ease-in-out"/>
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">John Doe</h1>
       <p className="text-gray-600 text-base sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
